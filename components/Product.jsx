f04b2ae23a60b902ca18ea746e41f3bfeeae10c0
Riddhick <riddhick14@gmail.com>
2023-10-31 14:31:03 +0530

    UI Changes: Button Position Changed

--- components/Product.jsx
+++ components/Product.jsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 export default function Product(props) {
   return (
     <div className='flex flex-col w-full px-6 gap-6 text-center'>
-        <div className='font-semibold'>{props.details.Title}</div>
+        <div className='font-semibold pt-4 '>{props.details.Title}</div>
         <div className='w-full flex flex-row justify-center'><Image
           src={props.details.ImageUrls}
           alt={props.details.Title}
@@ -11,11 +11,11 @@ export default function Product(props) {
           height={200} 
           className='shadow-black shadow-2xl rounded-xl'
         /></div>
-        <div className='flex flex-col '>
+        <div className='flex flex-col items-center'>
             <div>Current Price= {props.details.Currency}{props.details.CurrentPrice}</div>
             <div>Original Price= {props.details.Currency}{props.details.OriginalPrice}</div>
         </div>
-        <div className='flex flex-col gap-2 items-center'> 
+        <div className='flex flex-row gap-2 items-center justify-center'> 
           <button className='bg-black w-24 text-white p-2 rounded '><a href={props.details.Link}>Buy Now</a></button>
           <button className='bg-black w-24 text-white p-2 rounded'>Track</button>
         </div>
