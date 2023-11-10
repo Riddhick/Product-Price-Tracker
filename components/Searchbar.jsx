e88064b5ec3e3d2d3002d228070e1775a8d8ae89
Riddhick <riddhick14@gmail.com>
2023-11-10 20:05:51 +0530

    Feature Added : Loading Bar

--- components/Searchbar.jsx
+++ components/Searchbar.jsx
@@ -3,13 +3,16 @@ import { ScrapeAndStore } from "@/lib/actions";
 import { useState } from "react"
 import Product from "./Product";
 import Landing from "./Landing";
+import LoadingBar from "react-top-loading-bar";
 
 export default function Searchbar() {
   const [searchPrompt,setSearchPrompt]=useState('');
-  const [productDetails,setProductDetails]=useState(null)
+  const [productDetails,setProductDetails]=useState(null);
+  const [progress,setProgress]=useState(0);
   var product='null'
   const handleSubmit=async (event)=>{
     event.preventDefault()
+    setProgress(100);
     product=await ScrapeAndStore(searchPrompt)
    //console.log(product.Title)
     setProductDetails(product)
@@ -18,6 +21,7 @@ export default function Searchbar() {
 
   return (
     <>
+    <LoadingBar color="#000000" progress={progress} onLoaderFinished={()=>setProgress(0)} loaderSpeed={12000} height={3} waitingTime={5000}/>
     <form onSubmit={handleSubmit} className="m-2 px-2 flex flex-row justify-center gap-4">
         <input type="text" placeholder="Enter Product Link" value={searchPrompt} onChange={(e)=>setSearchPrompt(e.target.value)} className="lg:w-7/12 border-solid border-2 rounded px-2"></input>
         <button type="submit" className="bg-black text-white p-1 rounded">Search</button>
